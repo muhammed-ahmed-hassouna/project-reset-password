498b09f93d93f81151ce68e4f8226560f6b835c9
muhammed-ahmed-hassouna <142113275+muhammed-ahmed-hassouna@users.noreply.github.com>
2023-08-25 20:54:41 +0300

    Add files via upload

---
+++ Reset password.py
@@ -0,0 +1,88 @@
+#project:Forget the password
+M = "\033[1;30m "
+password = input (M+"Enter your password :") 
+
+if password == "Nn7" :
+    D = "\033[0;34m"
+    print (D+"Welcoem ")
+    print ("Good job..!")
+
+
+else:
+    V = "\033[0;31m"
+    print (V+"your forgot the password")
+    print ("To make sure you are not a robot")
+    print("9797")
+    M = input("What is this number===>:")
+    M = int(M)
+    T = "\033[1;94m "
+    Q = "\033[0;31m"
+    if M == 9797:
+      print(T+"welcome..!")
+      import os
+      os.system ("clear")
+    F = 9797
+    F = int(F)
+    if F == M:
+     print("Enter the new password")
+    if M != 9797:
+      print("Sorry, you cannot change the password. Try again later")
+    else:
+      O = input("Here==>:")
+      password = O
+      if password == O:
+        print("done..!")
+        import os
+        os.system ("clear")
+        Mmm = "\033[1;30m "
+        password = input (Mmm+"Enter your password:" )
+        if password == O:
+          D = "\033[0;34m"
+          print (D+"Welcoem Back..!") 
+        if password != O:
+          Pp = "\033[0;31m"
+          print(Pp+"Password wrong, please reset the password again")
+ 
+ 
+          Se = input("Type(8080)To try again==>:")
+          Se = int(Se)
+          if Se == 8080:
+            K = "\033[0;31m"
+            import os
+            os.system ("clear")
+            print (K+"your forgot the password")
+            print ("To make sure you are not a robot")
+          if Se != 8080:
+            print("Try again later")
+          else:
+            print("9999")
+            N = input("What is this number===>:")
+            N = int(N)
+            Ooo = "\033[1;94m "
+            Qq = "\033[0;31m"
+            if N == 9999:
+              print(Ooo+"welcome..!")
+              import os
+              os.system ("clear")
+              FF = 9999
+              FF = int(FF)
+              if FF == N:
+                print("Enter the new password")
+            if N != 9999:
+              print("Sorry, you cannot change the   password. Try again later")
+            else:
+              EE = input("Here==>:")
+              password = EE
+              if password == EE:
+                print("done..!")
+                import os
+                os.system ("clear")
+                Mmmm = "\033[1;30m "
+                password = input (Mmmm+"Enter your password:" )
+                if password == EE:
+                  R = "\033[0;34m"
+                  print (R+"Welcoem Back..!") 
+                if password != EE:
+                  Ppp = "\033[0;31m"
+                  print(Ppp+"Password wrong, please  reset the password again") 
+
